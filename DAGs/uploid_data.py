7613af4c9c4071cf390fb1680142161f3d3c0e46
Mohamed-Diouf <81381374+Mohamed-Diouf@users.noreply.github.com>
2024-05-16 11:26:54 +0200

    Update uploid_data.py

--- DAGs/uploid_data.py
+++ DAGs/uploid_data.py
@@ -1,14 +1,25 @@
 import os
 import json
 import time
+import logging
 import pandas as pd
-import psycopg2
-import os
-import time
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
 from urllib.parse import urlparse, parse_qs
 
 URL_FILE = "url.json"
 DATA_PATH = "data"
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 5, 16),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
 def check_environment_setup():
     # Configuration du logger
@@ -29,6 +40,24 @@ def check_environment_setup():
 
     logger.info("--" * 20)
 
+def load_data():
+    # Exemple d'utilisation des fonctions
+    url = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-logements-neufs/lines?size=10000&format=csv&after=10000%2C965634&header=true"
+
+    # Charger les données à partir de l'URL
+    data = load_data_from_url(url)
+
+    if data is not None:
+        # Traiter les données et obtenir le JSON
+        json_data = process_results(data)
+
+        # Enregistrer le JSON dans le répertoire de données local
+        data_filename = save_to_data_directory(json_data)
+
+        # Supprimer les doublons du fichier JSON dans le répertoire de données
+        if data_filename is not None:
+            drop_duplicates_from_data(data_filename)
+
 def load_data_from_url(url):
     """
     Charge les données à partir de l'URL spécifiée.
@@ -45,7 +74,6 @@ def load_data_from_url(url):
         return data
     except Exception as e:
         print(f"Erreur lors du chargement des données à partir de l'URL : {str(e)}")
-
 
 def process_results(data):
     """
@@ -74,9 +102,6 @@ def process_results(data):
     print(f"Data saved to {data_filename}")
     return json_data
 
-
-
-
 def save_to_data_directory(json_data):
     """
     Enregistre les données JSON dans le répertoire de données local.
@@ -104,7 +129,6 @@ def save_to_data_directory(json_data):
     except Exception as error:
         print(f"Erreur lors de l'enregistrement des données dans le répertoire de données : {error}")
         return None
-
 
 def drop_duplicates_from_data(data_filename):
     """
@@ -135,21 +159,31 @@ def drop_duplicates_from_data(data_filename):
         print(f"Erreur lors de la suppression des doublons dans le fichier JSON : {error}")
         return False
 
+# DAG Airflow
+with DAG('load_data', default_args=default_args, schedule_interval=timedelta(days=1)) as dag:
+    check_environment_setup_task = PythonOperator(
+        task_id='check_environment_setup',
+        python_callable=check_environment_setup
+    )
 
+    load_data_task = PythonOperator(
+        task_id='load_data',
+        python_callable=load_data
+    )
 
-# Exemple d'utilisation des fonctions
-url = "https://data.ademe.fr/data-fair/api/v1/datasets/dpe-v2-logements-neufs/lines?size=10000&format=csv&after=10000%2C965634&header=true"
-if __name__ == "__main__":
-    # Charger les données à partir de l'URL
-    data = load_data_from_url(url)
+    process_results_task = PythonOperator(
+        task_id='process_results',
+        python_callable=process_results
+    )
 
-    if data is not None:
-        # Traiter les données et obtenir le JSON
-        json_data = process_results(data)
+    save_to_data_directory_task = PythonOperator(
+        task_id='save_to_data_directory',
+        python_callable=save_to_data_directory
+    )
 
-        # Enregistrer le JSON dans le répertoire de données local
-        data_filename = save_to_data_directory(json_data)
+    drop_duplicates_task = PythonOperator(
+        task_id='drop_duplicates',
+        python_callable=drop_duplicates_from_data  # Assuming this function is for dropping duplicates from data file
+    )
 
-        # Supprimer les doublons du fichier JSON dans le répertoire de données
-        if data_filename is not None:
-           drop_duplicates_from_data(data_filename)
+    check_environment_setup_task >> load_data_task >> process_results_task >> save_to_data_directory_task >> drop_duplicates_task
